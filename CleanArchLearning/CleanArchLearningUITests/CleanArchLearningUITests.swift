8336252a35ce2412148ec13598f177e77632d0d5
saketmurari <saketmurari@gmail.com>
2022-10-08 18:01:49 +0530

    Folder restructing

--- CleanArchLearning/CleanArchLearningUITests/CleanArchLearningUITests.swift
+++ CleanArchLearning/CleanArchLearningUITests/CleanArchLearningUITests.swift
@@ -10,24 +10,24 @@ import XCTest
 class CleanArchLearningUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+    }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testExample() throws {
+        
+        let app = XCUIApplication()
+        let goStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Go"]/*[[".buttons[\"Go\"].staticTexts[\"Go\"]",".staticTexts[\"Go\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        XCTAssert(goStaticText.exists)
+        goStaticText.tap()
+        let backStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Back"]/*[[".buttons[\"Back\"].staticTexts[\"Back\"]",".staticTexts[\"Back\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        XCTAssert(backStaticText.exists)
+        
     }
 
     func testLaunchPerformance() throws {
